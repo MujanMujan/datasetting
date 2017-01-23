a989184e37e7fbf83dba33f2a60d6e86a65acbb7
Mujan <m.abbasi.72@gmail.com>
2017-01-23 03:47:44 +0330

    3 ta option ezafe shod

--- InteractiveModule.R
+++ InteractiveModule.R
@@ -60,13 +60,75 @@ convertDataType <- function(obj) {
 
 
 
+selectColumns <- function(obj) {
+  cat("\nThese are your columns name : ", names(obj), "\n")
+  user_input <- readline(prompt = "\nWhich columns you want to select ? enter the names by seprate it with coma (,) : ")
+  user_input <- strsplit(user_input, ",")
+  user_input <- user_input[[1]]
+  user_input <- str_trim(user_input)
+  
+  cat("columns you want to select :\n", user_input, "\n")
+  indecses_of_columns <- match(user_input, names(obj))
+  
+  obj <- obj[indecses_of_columns]
+  
+  return(obj)
+}
+
+
+selectRows <- function(obj) {
+  
+  
+  cat("\n1. select rows by numerical range\n2. select rows by condition")
+  user_choise <- readline(prompt = "1 or 2 ? ")
+  
+  if (user_choise == "1") {
+    start <- as.integer(readline(prompt = "enter the number of BEGIN the range of rows (like 1) : ") )
+    end <- as.integer(readline(prompt = "enter the number of END the range of rows (like 10) : ") )
+    ifile <- obj[start:end, ]
+    return(ifile)
+  } else if (user_choise == "2") {
+    
+    loop <- TRUE
+    while(loop) {
+      key <- readline(prompt = "enter your key column : ")
+      condition <- readline(prompt = "enter the condition like '>= 20'  : ")
+      cat("finally condition is : ", key , condition)
+      ok <- readline(prompt = "is it ok ? [yes/no] : ")
+      
+      if (ok == "no") {
+        loop <- TRUE
+      } else {
+        loop <- FALSE
+      }
+    }
+    condition <- strsplit(condition, " ")
+    condition_sign <- condition[[1]][1]
+    condition_value <- as.numeric(condition[[1]][2])
+    
+    switch (condition_sign,
+            "==" = {ifile <- obj[ obj [ , key] == condition_value, ]},
+            ">=" = {ifile <- obj[ obj [ , key] >= condition_value, ]},
+            "<=" = {ifile <- obj[ obj [ , key] <= condition_value, ]},
+            ">"  = {ifile <- obj[ obj [ , key] > condition_value, ]},
+            "<"  = {ifile <- obj[ obj [ , key] < condition_value, ]}
+    )
+    return(ifile) 
+    }
+}
+
+
+
+
+
+
 
 
 
 Mujan <- function() {
-  cat("Welcome to Interactive module to preparing dataset !\nCreator: Mujan\n")
+  cat("Welcome to Interactive module to preparing dataset !\nCreator: Mujan\n\n\nyour current directiry is", getwd())
   while(TRUE) {
-  cat("\n1. Import Data\n2. Report Data\n3. Convert Column's Type")
+  cat("\n1. Import Data\n2. Report Data\n3. Convert Column's Type\n4. Select Specific Columns\n5. Select Specific Rows\n0. exit")
   answer <- readline(prompt = "What is your choose : ")
   switch (answer,
           "1" = {datum <- importData()
@@ -89,9 +151,35 @@ Mujan <- function() {
                 } else if (x == "yes") {
                   my_dataset <<- convertDataType(datum)
                   print("the modified dataset returned in variable called 'my_dataset'")
-                }
-                  
+                }},
+          "4" = {x <- readline(prompt = "are you want work with your last dataset (my_dataset)? [yes/no] : ")
+                if (x == "no") {
+                  datum <- importData()
+                } else if (x == "yes") {
+                  my_dataset <- selectColumns(datum)
+                  user_answer <- readline(prompt = "do you want apply changes in 'my_dataset' ? ")
+                  if (user_answer == "yes") {
+                    my_dataset <<- my_dataset
+                  } else if (user_answer == "no") {
+                    new_cdataset <<- my_dataset
+                    cat("the new dataset (new_cdataset) was created !\n\n\n")
                   }
+                }},
+          "5" = {x <- readline(prompt = "are you want work with your last dataset (my_dataset)? [yes/no] : ")
+                if (x == "no") {
+                  datum <- importData()
+                } else if (x == "yes") {
+                  my_dataset <- selectRows(datum)
+                  user_answer <- readline(prompt = "do you want apply changes in 'my_dataset' ? ")
+                  if (user_answer == "yes") {
+                    my_dataset <<- my_dataset
+                  } else if (user_answer == "no") {
+                    new_rdataset <<- my_dataset
+                    cat("the new dataset (new_rdataset) was created !\n\n\n")
+                  }
+                }},
+          "0" = {cat("GoodBye :)")
+                 break}
   ) #Switch ended
 
   } #While loop ended
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # datasetting
 R's language module for preparing phase in datamining
+
+
+# Options
+
+this module can :
+1. Import Data
+2. Report Data ( all exploraty functions applied )
+3. Convert Column's Type
+4. Select Specific Columns
+5. Select Specific Rows ( both in condition and numerical range )
+
